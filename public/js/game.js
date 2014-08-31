951ccd96737916bb475ba6e909425ede1e42d291
kennw <koonwah@gmail.com>
2014-08-31 14:24:34 -0700

    uncommented closure on game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,4 @@
-// ;(function () {
+;(function () {
 
   var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'breakout', { preload: preload, create: create, update: update });
   var GAME_WIDTH = 800;
@@ -406,4 +406,4 @@
     return result;
   }
 
-// }());
+}());
