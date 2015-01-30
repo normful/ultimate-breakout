39533a6e509b717704943493af9c64a4b2eabf95
Norman Sue <normful@gmail.com>
2015-01-29 16:36:39 -0800

    add heroku git push helper script for LHL apps

---
+++ lhl-breakouts-heroku-push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git push lhl1 lhl-branding:master -f
+git push lhl2 lhl-branding:master -f
+git push lhl3 lhl-branding:master -f
+git push lhl4 lhl-branding:master -f
+git push lhl5 lhl-branding:master -f
+git push lhl6 lhl-branding:master -f
